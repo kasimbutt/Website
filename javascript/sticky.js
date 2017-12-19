6d48abe4783d394a2305becd6580bd4f3db6b523
kasimbutt <kasim_butt@hotmail.com>
2017-12-19 17:40:01 +0000

    Responsiveness/Mobile First
    Worked on Responsiveness of page by altering css and html to become mobile first.

--- javascript/sticky.js
+++ javascript/sticky.js
@@ -1,10 +1,13 @@
-var elementTop = $('.navbar').offset().top;
+$(function () {
 
-$(window).scroll(function() {
-	if( $(window).scrollTop() >= elementTop) {
-		$('body').addClass('nav_fixed');
-	} else {
-    $('body').removeClass('nav_fixed');
-  }
+		$(window).scroll(function(event) {
+			if($(this).scrollTop() > 150) {
+			$("#navbar").fadeIn();
+			$("#navbar").addClass('fixed',1000,'linear');
+		}
+		else {
+			$("#navbar").removeClass('fixed')
+		}
+		});
 
-});
+	});
